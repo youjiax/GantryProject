e84288939fb24e5ff03a235b576017eb45ccb556
MattWelc01 <79120136+MattWelc01@users.noreply.github.com>
2022-07-25 17:44:40 +1200

    Update sketch_jul25a.ino

--- sketch_jul25a/sketch_jul25a.ino
+++ sketch_jul25a/sketch_jul25a.ino
@@ -18,10 +18,13 @@ void setup() {
     pinMode(ENCA, INPUT);
     pinMode(ENCB, INPUT);
     attachInterrupt(digitalPinToInterrupt(ENCA), readEncoder, RISING); //INTERRUPT TRIGGERED ON RISING EDGE FOR ENCA, RUN READ ENCODER FUNC 
+
+
+ 
 }
 
 
-void readEncoder(){
+void readEncoder(){ //Anticlockwise incr pos
   int b = digitalRead(ENCB);
   if(b>0){
     pos++;
@@ -31,11 +34,35 @@ void readEncoder(){
 }
 
 
+//positive degrees = anticlockise
+void moveDegrees(int degrees){
+  int counts = degrees*86/15; //weird fracion is 2064/360
+  Serial.print(counts);
+  //int absPos = abs(pos%2064); //need absolute
+
+  int error = pos - counts;
+
+  int Kp = 1;
+  
+  if(error <= 0){ //rotate acw
+    digitalWrite(M1d, HIGH); //LOW clockwise, HIGH anticlockwise
+    analogWrite(M1p, error*Kp);   //PWM Speed Control 255 is high 0 is low 
+    
+  }
+
+  if(error > 0){//rotate cw
+    digitalWrite(M1d, LOW); //LOW clockwise, HIGH anticlockwise
+    analogWrite(M1p, error*Kp);   //PWM Speed Control 255 is high 0 is low 
+    
+  }
+}
+
+
 void loop() {
   
-  Serial.print(pos);
+  //Serial.print(counts);
   Serial.println();
+    moveDegrees(720);
 
-    digitalWrite(M1d, HIGH); //LOW clockwise, HIGH anticlockwise
-    analogWrite(M1p, 175);   //PWM Speed Control 255 is high 0 is low
+
 }
